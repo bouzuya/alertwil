416061a9968b543594e4e993a823d5fc8036c0ab
bouzuya <m@bouzuya.net>
2016-06-27 07:52:12 +0900

    s/testdouble/sinon/

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,13 @@
 import * as assert from 'power-assert';
 import beater from 'beater';
+import * as proxyquire from 'proxyquire';
+import * as sinon from 'sinon';
 
 const { test } = beater();
 
 test('index', () => {
+  assert(assert);
+  assert(proxyquire);
+  assert(sinon);
   assert(1 === 1);
 });
