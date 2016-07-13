fabfd6907d50a445636d9caf9332ad30e3c51aeb
bouzuya <m@bouzuya.net>
2016-07-13 23:43:24 +0900

    extract views

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,21 +1,28 @@
 import { G, C } from './koa';
+import {
+  alert as renderAlert,
+  alertResult as renderAlertResult
+} from './views';
 
 function* createAlert<T>(next: G<T>): G<G<T>> {
   const context: C & { params: { id: string }; } = this;
   const groupId = context.params.id;
-  context.response.body = 'POST /groups/:id/alerts groupId=' + groupId;
+  const alert = { groupId };
+  context.response.body = renderAlert(alert);
 }
 
 function* showAlert<T>(next: G<T>): G<G<T>> {
   const context: C & { params: { id: string }; } = this;
   const alertId = context.params.id;
-  context.response.body = 'GET /alerts/:id alertId=' + alertId;
+  const alert = { alertId };
+  context.response.body = renderAlert(alert);
 }
 
 function* createAlertResult<T>(next: G<T>): G<G<T>> {
   const context: C & { params: { id: string }; } = this;
   const alertId = context.params.id;
-  context.response.body = 'POST /alerts/:id/results alertId=' + alertId;
+  const result = { alertId };
+  context.response.body = renderAlertResult(result);
 }
 
 export { createAlert, showAlert, createAlertResult };
