8e68eb3702e5fb7cc8ae53e8335fa814ae0e7bbb
bouzuya <m@bouzuya.net>
2016-07-13 21:08:42 +0900

    fix controller

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,20 +1,20 @@
 import { G, C } from './koa';
 
 function* createAlert<T>(next: G<T>): G<G<T>> {
-  const groupId: string = this.params.id;
-  const context: C = this;
+  const context: C & { params: { id: string }; } = this;
+  const groupId = context.params.id;
   context.response.body = 'POST /groups/:id/alerts groupId=' + groupId;
 }
 
 function* showAlert<T>(next: G<T>): G<G<T>> {
-  const alertId: string = this.params.id;
-  const context: C = this;
+  const context: C & { params: { id: string }; } = this;
+  const alertId = context.params.id;
   context.response.body = 'GET /alerts/:id alertId=' + alertId;
 }
 
 function* createAlertResult<T>(next: G<T>): G<G<T>> {
-  const alertId: string = this.params.id;
-  const context: C = this;
+  const context: C & { params: { id: string }; } = this;
+  const alertId = context.params.id;
   context.response.body = 'POST /alerts/:id/results alertId=' + alertId;
 }
 
