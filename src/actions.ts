33ae24683106659a94cb7547ed1c184a1fcb3de0
bouzuya <m@bouzuya.net>
2016-07-15 23:48:43 +0900

    extract AlertApplicationService

--- src/actions.ts
+++ src/actions.ts
@@ -13,33 +13,53 @@ import {
   AlertRepositoryImpl
 } from './repositories';
 
+class AlertApplicationService {
+  private repository: AlertRepository;
+
+  constructor(repository: AlertRepository) {
+    this.repository = repository;
+  }
+
+  createAlert(groupIdString: string): string {
+    const groupId = new GroupId(groupIdString);
+    const alertId = this.repository.nextId();
+    const alert = new Alert({ id: alertId, groupId });
+    alert.call();
+    this.repository.save(alert);
+    return renderAlert(alert);
+  }
+
+  showAlert(alertIdString: string): string {
+    const alertId = new AlertId(alertIdString);
+    const alert = this.repository.findBy({ alertId });
+    return renderAlert(alert);
+  }
+
+  createAlertResult(alertIdString: string): string {
+    const alertId = new AlertId(alertIdString);
+    const alert = this.repository.findBy({ alertId });
+    const result = alert.add('completed');
+    this.repository.save(alert);
+    return renderAlertResult(result);
+  }
+}
+
 function* createAlert<T>(next: G<T>): G<G<T>> {
   const context: C & { params: { id: string }; } = this;
-  const groupId = new GroupId(context.params.id);
-  const repository: AlertRepository = new AlertRepositoryImpl();
-  const alertId = repository.nextId();
-  const alert = new Alert({ id: alertId, groupId });
-  alert.call();
-  repository.save(alert);
-  context.response.body = renderAlert(alert);
+  const service = new AlertApplicationService(new AlertRepositoryImpl());
+  context.response.body = service.createAlert(context.params.id);
 }
 
 function* showAlert<T>(next: G<T>): G<G<T>> {
   const context: C & { params: { id: string }; } = this;
-  const alertId = new AlertId(context.params.id);
-  const repository: AlertRepository = new AlertRepositoryImpl();
-  const alert = repository.findBy({ alertId });
-  context.response.body = renderAlert(alert);
+  const service = new AlertApplicationService(new AlertRepositoryImpl());
+  context.response.body = service.showAlert(context.params.id);
 }
 
 function* createAlertResult<T>(next: G<T>): G<G<T>> {
   const context: C & { params: { id: string }; } = this;
-  const alertId = new AlertId(context.params.id);
-  const repository: AlertRepository = new AlertRepositoryImpl();
-  const alert = repository.findBy({ alertId });
-  const result = alert.add('completed');
-  repository.save(alert);
-  context.response.body = renderAlertResult(result);
+  const service = new AlertApplicationService(new AlertRepositoryImpl());
+  context.response.body = service.createAlertResult(context.params.id);
 }
 
 export { createAlert, showAlert, createAlertResult };
