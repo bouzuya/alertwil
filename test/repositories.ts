40380c35d539eb3eda9ac25b8155086f7086cf28
bouzuya <m@bouzuya.net>
2016-07-16 15:42:36 +0900

    add test for repositories

---
+++ test/repositories.ts
@@ -0,0 +1,18 @@
+import * as assert from 'power-assert';
+import beater from 'beater';
+import * as proxyquire from 'proxyquire';
+import * as sinon from 'sinon';
+import { Alert, GroupId } from '../src/models';
+import { AlertRepositoryImpl } from '../src/repositories';
+
+const { test } = beater();
+
+test('repositories', () => {
+  const repository = new AlertRepositoryImpl();
+  const groupId = new GroupId('456');
+  const alertId = repository.nextId();
+  const alert = new Alert({ id: alertId, groupId })
+  repository.save(alert);
+  const found = repository.findBy({ alertId: alertId });
+  assert.deepEqual(found, alert);
+});
