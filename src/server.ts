4e0ab72f2aca0f8142030e55f52e3681a623b087
bouzuya <m@bouzuya.net>
2016-07-18 23:05:31 +0900

    use koa-logger

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,15 @@
 import * as bodyParser from 'koa-bodyparser';
-import { koa, G } from './koa';
+import * as koaLogger from 'koa-logger';
+import { koa } from './koa';
 import { routes } from './routes';
 import { actions } from './actions';
 
 const server = (): void => {
   const app = koa();
-
-  // logger
-  app.use(function* <T>(next: G<T>): G<G<T>> {
-    const start = new Date().getTime();
-    yield next;
-    const ms = new Date().getTime() - start;
-    console.log('%s %s - %s', this.method, this.url, ms);
-  });
-
+  app.use(koaLogger());
   app.use(bodyParser());
-
   app.use(routes());
-
   app.use(actions());
-
   app.listen(3000);
 };
 
